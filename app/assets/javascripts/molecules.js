fa005ea343b1d4c1b29832e033f627d05beeff70
André Taiar <andre.taiar@gmail.com>
2016-10-13 01:33:07 -0300

    Small interface fixes.

--- app/assets/javascripts/molecules.js
+++ app/assets/javascripts/molecules.js
@@ -1,10 +1,8 @@
-$(document).ready(function(){
-  $('.add-smarts').on('click', function(){
-    types = [];
-    types['carboxilico']  = "[CX3](=O)[OX2H1]";
-    types['amina']        = "[NX3;H2,H1;!$(NC=O)]";
-    types['amida']        = "[CX3]=[OX1]";
-
-    $('input#smarts').val(types[$(this).data('smarts-type')]);
-  });
-});
+function add_smarts(obj) {
+  types = [];
+  types['carboxilico']  = "[CX3](=O)[OX2H1]";
+  types['amina']        = "[NX3;H2,H1;!$(NC=O)]";
+  types['amida']        = "[CX3]=[OX1]";
+  console.log(obj);
+  $('input#smarts').val(types[$(obj).data('smarts-type')]);
+}
