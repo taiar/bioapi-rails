2215c3ff8b50f536d82804d88d088a2225140e0a
André Taiar <andre.taiar@gmail.com>
2016-10-29 17:26:49 -0200

    HBA filter.

--- app/services/base_service/filters/hydrogen_bond_acceptor.rb
+++ app/services/base_service/filters/hydrogen_bond_acceptor.rb
@@ -1,2 +1,40 @@
 class BaseService::Filters::HydrogenBondAcceptor < BaseService::Filters::Filter
+
+  HBA_SMARTS = '[!$([#6,F,Cl,Br,I,o,s,nX3,#7v5,#15v5,#16v4,#16v6,*+1,*+2,*+3])]'
+
+  def initialize(params)
+    @lower = params[:hydrogen_bond_acceptor_start].presence
+    @upper = params[:hydrogen_bond_acceptor_end].presence
+  end
+
+  def valid?(mol)
+    @bond_acceptors = nil
+    super(mol)
+  end
+
+  def lower_bound
+    unless @lower.nil?
+      bond_acceptors >= @lower.to_i
+    else
+      true
+    end
+  end
+
+  def upper_bound
+    unless @upper.nil?
+      bond_acceptors <= @upper.to_i
+    else
+      true
+    end
+  end
+
+  def self.hba(mol)
+    mol.matches(BaseService::Filters::HydrogenBondAcceptor::HBA_SMARTS).count
+  end
+
+  private
+
+  def bond_acceptors
+    @bond_acceptors ||= BaseService::Filters::HydrogenBondAcceptor.hba(@mol)
+  end
 end
