cc5c3f143f3d04d12c1e63e0d36e36729d0426a2
André Taiar <andre.taiar@gmail.com>
2016-10-30 19:31:44 -0200

    Reaction service going.

--- app/services/reaction_service.rb
+++ app/services/reaction_service.rb
@@ -1,11 +1,44 @@
 class ReactionService
 
-  attr_accessor :reaction
+  attr_accessor :reaction, :base
 
   def initialize
     @base_dir = Rails.root.join('app/services/reaction_service')
     @zinc_dir = @base_dir.join('vendor/zinc')
     @reactor = @zinc_dir.join('reactsmi.py')
+    @tmp_dir =  Rails.root.join('tmp/reactions')
+    @reaction_id = SecureRandom.uuid
+    @reaction_dir = @tmp_dir.join(@reaction_id)
+    @base = []
+    @base_pos = 0
+    check_reaction_dir
+  end
+
+  def check_reaction_dir
+    unless File.directory?(@tmp_dir)
+      Dir.mkdir(@tmp_dir)
+    end
+    unless File.directory?(@tmp_dir)
+      Dir.mkdir(@reaction_dir)
+    end
+  end
+
+  def add_base(content)
+    @base[@base_pos] = content
+    write_base
+    @base_pos += 1
+  end
+
+  def write_base
+    File.open(@reaction_dir.join(@base_pos.to_s).to_s, 'w') { |file| file.write(@base[@base_pos]) }
+  end
+
+  def run
+    `#{@reactor.to_s} '#{@reaction}' #{@reaction_dir}/0 #{@reaction_dir}/1`
+  end
+
+  def amide_formation
+    '[C:1](=[O:2])[Cl:3].[H:99][NH:4][C:0]>> [C:1](=[O:2])[NH:4][C:0].[Cl:3][H:99]'
   end
 
   def example
