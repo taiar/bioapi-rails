5826d3d02ed00e8f6e4261cd96131782f7ac284b
André Taiar <andre.taiar@gmail.com>
2016-12-26 15:34:54 -0200

    Generating reactions sheet.

--- app/services/reaction_service/spreadsheet.rb
+++ app/services/reaction_service/spreadsheet.rb
@@ -9,6 +9,20 @@ class ReactionService::Spreadsheet
       p.workbook.add_worksheet(:name => "Base 1") { |sheet| add_base_sheet(sheet, @bases[0]) }
       p.workbook.add_worksheet(:name => "Base 2") { |sheet| add_base_sheet(sheet, @bases[1]) }
       p.workbook.add_worksheet(:name => "Reactions") do |sheet|
+        reactions = @raw_run.split("\n")
+        sheet.add_row ["Product molecule", "Base 1 molecule ID", "Base 2 molecule ID",
+          'LogP', 'Mass', 'Atoms number', 'Rings number', 'HBA', 'HBD']
+        reactions.each do |r|
+          reaction = r.split(" ")
+          references = reaction[1].split(".")
+
+          mol = Rubabel[reaction[0]]
+          sheet.add_row [reaction[0], references[0], references[1],
+            '%.5f' % BaseService::Filters::LogP.logp(mol),
+            '%.2f' % mol.exact_mass, mol.count, mol.ob_sssr.count,
+            BaseService::Filters::HydrogenBondDonor.hbd(mol),
+            BaseService::Filters::HydrogenBondAcceptor.hba(mol)]
+        end
       end
       p.serialize('simple.xlsx')
     end
